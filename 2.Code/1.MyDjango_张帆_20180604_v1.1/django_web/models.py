9e106c6dff78bf34741219eaeed89c71c0309b3a
Fairy1018 <2712709430@qq.com>
2018-06-04 20:03:54 +0800

    增加了图表信息

---
+++ 2.Code/1.MyDjango_张帆_20180604_v1.1/django_web/models.py
@@ -0,0 +1,35 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class Qcwytable(models.Model):
+    key = models.CharField(primary_key=True, max_length=100)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    link = models.CharField(max_length=200, blank=True, null=True)
+    company = models.CharField(max_length=100, blank=True, null=True)
+    salary = models.CharField(max_length=20, blank=True, null=True)
+    updatetime = models.CharField(max_length=20, blank=True, null=True)
+    salary_range = models.CharField(max_length=30, blank=True, null=True)
+    num = models.CharField(max_length=10, blank=True, null=True)
+    parent_link = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'qcwytable'
+
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=20, blank=True, null=True)
+    email = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user'
